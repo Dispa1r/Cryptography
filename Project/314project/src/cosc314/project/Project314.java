4f0ada1e0604224cc76a5f96025ec990cff11678
wthorn3 <31716723+wthorn3@users.noreply.github.com>
2017-12-07 12:50:05 -0500

    Add files via upload

---
+++ Project/314project/src/cosc314/project/Project314.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Project314 {
+
+  public static void main (String[] args) {
+    System.out.println("For DLP:");
+    System.out.println("x is: " + solveDLP(17, 3, 12));
+    System.out.println("For num conversion:");
+    System.out.println("The result of numToText(3781): " + numToText(17575));
+  }
+  
+  //q is a prime, a is a primitive root of q, y is a residue mod q
+  public static int solveDLP(int q, int a, int y) {
+    int x = -1;
+    int i = 1;
+    while (x != y) {
+      x = (int) Math.pow(a, i);
+      x = reduceByMod(x, q);
+      i++;
+    }
+    return i;
+  }
+  
+  //a is the number, b is the mod
+  public static int reduceByMod(int a, int b) {
+    int c = a;
+    if (a > b) {
+      c = a / b;
+      c = a - (c * b);
+    }
+    return c;
+  }
+  
+  
+  //Takes an integer and finds that corresponding 3 letter text
+  public static String numToText(int a) {
+    //x * 26^2 + y * 26 + z = a
+    //z is just a mod 26
+    int z = reduceByMod(a, 26);
+    System.out.println("Z is: " + z);
+    //y is a - z (mod 26^2)/26
+    int y = (reduceByMod(a, 676) - z)/26;
+    System.out.println("y is: " + y);
+    //Solve the original for x
+    int x = (a - (y*26 + z))/676;
+    System.out.println("x is: " + x);
+    return getCharacter(x) + getCharacter(y) + getCharacter(z);
+  }
+  
+  //Returns the number block using 3 letters
+  /*public static int textToNum(char a, char b, char c) {
+    int block = getCharCode(a) * 26^2 + getCharCode(b) * 26 + getCharCode(c);
+    return block;
+  }*/
+  
+  //Gets the integer value that corresponds with a character (a = 0, b = 1, etc...)
+  public static String getCharacter(int num) {
+    int letter = 'a' + num;
+    return Character.toString((char)letter);
+  }
+}
