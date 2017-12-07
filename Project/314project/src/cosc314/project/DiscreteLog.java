dfd75aff5437e9744d44b079f64c62b78fe6e708
wthorn3 <31716723+wthorn3@users.noreply.github.com>
2017-12-07 12:49:05 -0500

    Update DiscreteLog.java

--- Project/314project/src/cosc314/project/DiscreteLog.java
+++ Project/314project/src/cosc314/project/DiscreteLog.java
@@ -2,8 +2,26 @@ package cosc314.project;
 
 public class DiscreteLog {
 
-	public DiscreteLog() {
-		// TODO Auto-generated constructor stub
+	//q is prime, a is a primitive root of q, y is a residue mod q
+	public static int DiscreteLog(int q, int a, int y) {
+		int x = -1;
+    		int i = 1;
+    		while (x != y) {
+      			x = (int) Math.pow(a, i);
+      			x = reduceByMod(x, q);
+      			i++;
+    		}
+    		return i;
 	}
+	
+	//a is the number, b is the mod
+	public static int reduceByMod(int a, int b) {
+    		int c = a;
+   		if (a > b) {
+      			c = a / b;
+      			c = a - (c * b);
+    		}
+    		return c;
+  }
 
 }
